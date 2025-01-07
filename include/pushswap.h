34d859c7d9c6ffeda176bd6bd4ecaae25fcfb2c9
Tsubasa Ishihara <tishihar@c6r2s3.42tokyo.jp>
2025-01-07 17:51:41 +0900

    make struct

--- include/pushswap.h
+++ include/pushswap.h
@@ -1,16 +1,34 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   pushswap.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tishihar <tishihar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/01/07 17:35:23 by tishihar          #+#    #+#             */
-/*   Updated: 2025/01/07 17:45:29 by tishihar         ###   ########.fr       */
+/*   Created: 2025/01/07 16:49:38 by tishihar          #+#    #+#             */
+/*   Updated: 2025/01/07 16:58:09 by tishihar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	main(int argc, char **argv)
+#ifndef PUSHSWAP_H
+# define PUSHSWAP_H
+
+typedef struct s_node
 {
-	
-}
+	int				value;
+
+	struct s_node	prev;
+	struct s_node	next;
+}	t_node;
+
+typedef struct s_stack
+{
+	t_node	*top;
+	t_node	*bottom;
+	int		size;
+}	t_stack
+
+
+
+
+#endif
